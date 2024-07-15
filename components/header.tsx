fdd42a87e532e3cc82340f45c940fcde5ef24a91
Lewe Maiga <badraliyoumaiga199@gmail.com>
2024-07-15 15:30:10 +0100

    add box reveal animation

--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,40 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { CONFIG } from "@/data/config";
+import BoxReveal from "./box-reveal";
 import BlurFade from "./blur-fade";
 
-const BLUR_FADE_DELAY = 0.2;
+const BOX_REVEAL_DURATION = 0.5;
+
 export const Header = () => {
 	return (
 		<header className="max-w-2xl px-4">
 			<div className="flex flex-col gap-8">
-				<BlurFade delay={BLUR_FADE_DELAY}>
-					<div className="flex items-center gap-4">
+				<div className="flex items-center gap-4">
+					<BoxReveal boxColor="#15c16e" duration={BOX_REVEAL_DURATION}>
 						<Avatar className="size-24">
 							<AvatarImage src={CONFIG.avatarUrl} alt={CONFIG.name} />
 							<AvatarFallback>{CONFIG.initials}</AvatarFallback>
 						</Avatar>
-						<div className="space-y-1">
+					</BoxReveal>
+					<div className="space-y-1">
+						<BoxReveal boxColor="#15c16e" duration={BOX_REVEAL_DURATION}>
 							<h1 className="scroll-m-20 text-lg md:text-3xl font-extrabold tracking-tight">{CONFIG.fullname}</h1>
+						</BoxReveal>
+						<BoxReveal boxColor="#15c16e" duration={BOX_REVEAL_DURATION}>
 							<p className="text-sm text-muted-foreground">{CONFIG.bio}</p>
-							<div>
+						</BoxReveal>
+						<div>
+							<BoxReveal boxColor="#15c16e" duration={BOX_REVEAL_DURATION}>
 								<div className="gap-2 inline-flex items-center bg-green-100 rounded-full px-3 py-0.5 text-sm font-medium text-green-800 dark:text-green-50 dark:bg-green-400">
 									<span className="inline-flex size-2 rounded-full bg-green-400 dark:bg-green-50"></span>
 									Available for work
 								</div>
-							</div>
+							</BoxReveal>
 						</div>
 					</div>
-				</BlurFade>
+				</div>
 				<section className="flex flex-col gap-4">
-					<BlurFade delay={BLUR_FADE_DELAY * 2}>
+					<BlurFade>
 						<h3 className="font-medium">About me</h3>
 						<p className="text-muted-foreground leading-7">{CONFIG.summary}</p>
 					</BlurFade>
