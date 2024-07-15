852ddd71191364b4f40194def90950839cd590b4
Lewe Maiga <badraliyoumaiga199@gmail.com>
2024-07-15 14:56:11 +0100

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,12 +5,16 @@ const WEBSITE_HOST_URL = "https://www.lewe.dev";
 export default async function sitemap() {
 	const allPosts = posts.getPages().map((post) => ({
 		url: `${WEBSITE_HOST_URL}/${post.slugs[0]}`,
-		lastModified: post.data.date.toISOString().split("T")[0],
+		lastModified: new Date(post.data.exports.lastModified || post.data.date).toISOString().split("T")[0],
+		changeFrequency: "weekly",
+		priority: 0.5,
 	}));
 
 	const routes = ["", "posts"].map((route) => ({
 		url: `${WEBSITE_HOST_URL}/${route}`,
 		lastModified: new Date().toISOString().split("T")[0],
+		changeFrequency: "monthly",
+		priority: 1,
 	}));
 
 	return [...routes, ...allPosts];
