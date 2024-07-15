cd841710129d7794f67cee600cd3799bc6aa0f0a
Lewe Maiga <badraliyoumaiga199@gmail.com>
2024-07-15 19:07:02 +0100

    update page fade animation delay

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { CONFIG } from "@/data/config";
 import { compareDesc } from "date-fns";
 import Link from "next/link";
 
-const BLUR_FADE_DELAY = 0.04;
+const BLUR_FADE_DELAY = 0.1;
 
 export default function Home() {
 	const sortedPost = posts
