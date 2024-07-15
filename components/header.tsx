2c55f24533b49ef70d2301954973a462f3b1f63e
Lewe Maiga <badraliyoumaiga199@gmail.com>
2024-07-15 16:22:04 +0100

    update animation delay

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,7 @@ import { CONFIG } from "@/data/config";
 import BlurFade from "./blur-fade";
 import BoxReveal from "./box-reveal";
 
-const BOX_REVEAL_DURATION = 0.04;
+const BOX_REVEAL_DURATION = 0.5;
 
 export const Header = () => {
 	return (
